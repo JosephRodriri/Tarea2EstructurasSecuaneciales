4cccdd9098615a8ea760230476a8d7abfc0087ab
Joseph Rodriguez <josephrodriri@gmail.com>
2024-05-24 22:12:20 -0500

    Tarea2EstructurasSecuaneciales

---
+++ script.js
@@ -0,0 +1,11 @@
+let pulgadas= 0;
+let metros=0;
+let resultados=0;
+
+metros=Number(prompt('Digite la cantidad cuantos metros requiere'));
+
+resultados=metros*39.3701;
+
+alert('la cantidad de pulgadas en tela que debe pedir es de: ' +resultados);
+
+
